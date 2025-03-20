b08acb3d3df8ea4f809af26e758dc19c99528a57
CameronCarter1 <112524542+CameronCarter1@users.noreply.github.com>
2025-03-20 19:50:13 -0400

    Created the audiobook file

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/Audiobook.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.midterm.question5;
+
+public class Audiobook implements Streamable {
+    private String title;
+
+    public Audiobook(String title) { this.title = title; }
+
+    public void play() { System.out.println("Playing audiobook: " + title); }
+    public void pause() { System.out.println("Paused audiobook: " + title); }
+    public void stop() { System.out.println("Stopped audiobook: " + title); }
+
+    public void setPlaybackSpeed(double speed) {
+        System.out.println("Setting playback speed: " + speed + "x");
+    }
+}
+
+
