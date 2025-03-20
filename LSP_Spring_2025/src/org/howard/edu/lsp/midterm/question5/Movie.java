ec4b50d9c0ac2a8e7d18fbdd5e71558d98070405
CameronCarter1 <112524542+CameronCarter1@users.noreply.github.com>
2025-03-20 19:48:46 -0400

    created the movie and music java file

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/Movie.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.midterm.question5;
+
+public class Movie implements Streamable {
+    private String title;
+
+    public Movie(String title) { this.title = title; }
+
+    public void play() { System.out.println("Playing movie: " + title); }
+    public void pause() { System.out.println("Paused movie: " + title); }
+    public void stop() { System.out.println("Stopped movie: " + title); }
+
+    public void rewind(int seconds) {
+        System.out.println("Rewinding movie: " + title + " by " + seconds + " seconds");
+    }
+}
+
---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question5/Music.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.midterm.question5;
+
+public class Music implements Streamable {
+    private String title;
+
+    public Music(String title) { this.title = title; }
+
+    public void play() { System.out.println("Playing music: " + title); }
+    public void pause() { System.out.println("Paused music: " + title); }
+    public void stop() { System.out.println("Stopped music: " + title); }
+
+    public void addToPlaylist(String playlist) {
+        System.out.println("Added " + title + " to " + playlist);
+    }
+}
