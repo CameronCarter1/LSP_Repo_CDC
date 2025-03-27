55142303303f039743f79fc2f039b0e315ef02d3
CameronCarter1 <112524542+CameronCarter1@users.noreply.github.com>
2025-03-26 22:34:14 -0400

    tried to test clear and add methods. Encountered problems on the integerse

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment5/Driver.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment5/Driver.java
@@ -1,5 +1,14 @@
 package org.howard.edu.lsp.assignment5;
 
 public class Driver {
-
+	public static void main(String[] args) {
+		// Test clear and add
+		IntegerSet set1 = new IntegerSet();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		System.out.println("Set1 after adds: " + set1);
+		set1.clear();
+		System.out.println("Set1 after clear: " + set1);
+	}
 }
