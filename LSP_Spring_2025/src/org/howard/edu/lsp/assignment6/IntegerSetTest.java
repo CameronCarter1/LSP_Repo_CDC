58b4cf4949a5c4cc10226b3473fdaf02602b37b5
CameronCarter1 <112524542+CameronCarter1@users.noreply.github.com>
2025-03-31 20:43:26 -0400

    Created IntegerSetTest and package for assignment 6

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment6/IntegerSetTest.java
@@ -0,0 +1,94 @@
+package org.howard.edu.lsp.assignment6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class IntegerSetTest {
+
+	@Test
+	@DisplayName("Test case for clear")
+	void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for length")
+	void testLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for Equals")
+	void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for contains")
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for largest and execption")
+	void testLargest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for smallest and exception")
+	void testSmallest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for add")
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for remove")
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for union")
+	void testUnion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for intersect")
+	void testIntersect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for difference")
+	void testDiff() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for complement")
+	void testComplement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for isEmpty")
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName("Test case for to string")
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
